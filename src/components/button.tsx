90587fb47a9e86b80da2af4c6443327753d83a44
LuizGarbini <garbasneto@gmail.com>
2025-04-29 18:09:16 -0300

    Rest Operator

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,6 @@ type Props = React.ComponentProps<"button"> & {
 	name: string;
 };
 
-export function Button({ name, onClick }: Props) {
-	// biome-ignore lint/a11y/useButtonType: <explanation>
-	return <button onClick={onClick}>{name}</button>;
+export function Button({ name, ...rest }: Props) {
+	return <button {...rest}>{name}</button>;
 }
