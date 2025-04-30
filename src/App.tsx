bbd35eba2610e45c061dfc31e24d8dca23ccfedc
LuizGarbini <garbasneto@gmail.com>
2025-04-30 03:11:33 -0300

    UseState na Prática

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import "./global.css";
 
 import { Button } from "./components/button";
 
 import styles from "./app.module.css";
 
-import { useMessage } from "./hooks/use-message";
+// import { useMessage } from "./hooks/use-message";
 
 export function App() {
-	const { show } = useMessage({ age: 22, name: "Luiz Garbini" });
+	const [count, setCount] = useState(0);
+
+	// const { show } = useMessage({ age: 22, name: "Luiz Garbini" });
+
+	function handleAdd() {
+		setCount(count + 1);
+	}
+
+	function handleRemove() {
+		setCount(count - 1);
+	}
 
 	return (
 		<div className={styles.container}>
-			<Button
-				name="Adicionar"
-				onClick={() => show("Mensagem personalizada do meu hook")}
-			/>
-			<span>0</span>
-			<Button name="Remover" />
+			<Button name="Adicionar" onClick={handleAdd} />
+			<span>{count}</span>
+			<Button name="Remover" onClick={handleRemove} />
 		</div>
 	);
 }
